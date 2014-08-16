ca3c2372630132085af1e729d1cb6435698a0efb
buggyj <buggyjef@gmail.com>
2014-08-16 09:56:14 +0200

    working

--- edit-json.js
+++ edit-json.js
@@ -1,7 +1,7 @@
 /*\
-title: $:/plugins/bj/jsoneditor/edit-json.js
+title: $:/plugins/bj/jsoneditor/jsoneditor.js
 type: application/javascript
-module-type: widget
+module-type: startup
 
 jsoneditor adaptor
 
@@ -14,74 +14,56 @@ jsoneditor adaptor
 
 var MIN_TEXT_AREA_HEIGHT = 100; // Minimum height of textareas in pixels
 
-var Widget = require("$:/core/modules/widgets/widget.js").widget;
-var newid=0;
-var EditJsonWidget = function(parseTreeNode,options) {
-	this.initialise(parseTreeNode,options);
-};
+// Export name and synchronous status
+exports.name = "json-editor";
+exports.platforms = ["browser"];
+exports.synchronous = true;
 if($tw.browser) {
 	require("$:/plugins/bj/jsoneditor/JSONeditor.js");
 }
-/*
-Inherit from the base widget class
-*/
-EditJsonWidget.prototype = new Widget();
+var instance;
 
-/*
-Render this widget into the DOM
-*/
-EditJsonWidget.prototype.render = function(parent,nextSibling) {
-	var self = this;
-	// Save the parent dom node
-	this.parentDomNode = parent;
-	// Compute our attributes
-	this.computeAttributes();
-	// Execute our logic
-	this.execute();
-	// Create our element
-	var editInfo = this.getEditInfo();
-	var domNode = this.document.createElement("div");
-	var domNode2 = this.document.createElement("div");
-	if(this.editType) {
-		domNode.setAttribute("type",this.editType);
+var newid =0;
+$tw.wiki.getTiddlerText = function(title,defaultText) {
+	var tiddler = this.getTiddler(title);
+	// Return undefined if the tiddler isn't found
+	if(!tiddler) {
+		return defaultText;
 	}
-	if(editInfo.value === "" && this.editPlaceholder) {
-		domNode.setAttribute("placeholder",this.editPlaceholder);
+	if(tiddler.fields.text !== undefined) {
+		// Just return the text if we've got it
+		return tiddler.fields.text;
+	} else {
+		// Tell any listeners about the need to lazily load this tiddler
+		this.dispatchEvent("lazyLoad",title);
+		// Indicate that the text is being loaded
+		return null;
 	}
-	// Assign classes
-	if(this.editClass) {
-		domNode.className = this.editClass;
-	}
-	var domNode2 = this.document.createElement("div");
+};
+exports.startup  = function() {
+
+	var domNode = document.createElement("div");
+	var domNode2 =document.createElement("div");
+    		document.body.insertBefore(domNode,document.body.firstChild);
+    		    		document.body.insertBefore(domNode2,document.body.firstChild);
 	domNode.innerHTML = '<div style="font-size: 11px; font-family: Verdana,Arial,Helvetica,sans-serif;" id="jsoneditortree'+newid+'"></div>';
-	domNode2.innerHTML 	=	this.formHTML;
+	domNode2.innerHTML 	=	formHTML;
 	
-	// Add an input event handler
-	//$tw.utils.addEventListeners(domNode2,[
-	//	{name: "focus", handlerObject: this, handlerMethod: "handleFocusEvent"},
-	//	{name: "input", handlerObject: this, handlerMethod: "handleInputEvent"}
-	//]);
 
-	// Insert the element into the DOM
-	parent.insertBefore(domNode2,nextSibling);
-	parent.insertBefore(domNode,domNode2);
 	var updater =function(x){
-		self.saveChanges(x);
+		alert("updater!");
 	}
-	this.instance=JSONeditor.start('jsoneditortree'+newid,domNode2.firstChild.firstChild,JSON.parse(editInfo.value),
-									'$:/plugins/bj/jsoneditor/',this.format,updater);
-	var instance = this.instance;
+	var test ={testing:"val"};
+	instance=JSONeditor.start('jsoneditortree'+newid,domNode2.firstChild.firstChild,$tw.wiki.allTitles(),
+									'$:/plugins/bj/jsoneditor/','',updater);
+
 
 	newid++;
-	this.domNodes.push(domNode);
-	this.domNodes.push(domNode2);
-	if(this.postRender) {
-		this.postRender();
-	}
-	this.formsetup(domNode2.firstChild);
+
+	formsetup(domNode2.firstChild);
 };
 
-EditJsonWidget.prototype.formHTML=	
+var formHTML=	
 	'<div style="font-size: 11px; font-family: Verdana,Arial,Helvetica,sans-serif;" ">'+
 	"<form name=\"jsoninput\" >"	+
 	"\nLabel:<br>"+
@@ -107,8 +89,7 @@ EditJsonWidget.prototype.formHTML=
 	"\n<input type=\"checkbox\" name=\"jbefore\">Add children first/siblings before\n<br>"+
 	"\n<input type=\"checkbox\" name=\"jPasteAsChild\">Paste as child on objects & arrays\n<br><br><div id=\"jformMessage\"></div>\n</form></div>";
 				
-EditJsonWidget.prototype.formsetup = function(f) {
-	var instance = this.instance
+var formsetup = function(f) {
 	var fs=f.style
 	fs.fontSize=fs.fontSize="11px"
 	fs.fontFamily=fs.fontFamily="Verdana,Arial,Helvetica,sans-serif"
@@ -121,26 +102,26 @@ EditJsonWidget.prototype.formsetup = function(f) {
 		}
 		var cb= e[i].name
 		if (!!cb ) switch (cb) {
-			case 'jsoninput': if (this.onkeyupdate!=="yes") e[i].addEventListener("submit", function (e) {
+			case 'jsoninput': if (false) e[i].addEventListener("submit", function (e) {
 				e.preventDefault();
 				instance.jsonChange(e.target);
 				return false;
 			});
 
 			break;
-			case 'jlabel': if (this.onkeyupdate==="yes") e[i].addEventListener("input", function (e) {
+			case 'jlabel': if (false) e[i].addEventListener("input", function (e) {
 				//e.preventDefault();
 				instance.jsonChange(e.target.parentNode);
 				return false;
 			});
 			break;
-			case 'jvalue': if (this.onkeyupdate==="yes") e[i].addEventListener("input", function (e) {
+			case 'jvalue': if (false) e[i].addEventListener("input", function (e) {
 				//e.preventDefault();
 				instance.jsonChange(e.target.parentNode);
 				return false;
 			});
 			break;
-			case 'jsonUpdate': if (this.onkeyupdate==="yes") e[i].style.display = 'none';
+			case 'jsonUpdate': if (false) e[i].style.display = 'none';
 			break;
 			case 'jtype': e[i].addEventListener("change", function (e) {
 				instance.changeJsonDataType(e.target.value,e.target.parentNode);
@@ -178,131 +159,7 @@ EditJsonWidget.prototype.formsetup = function(f) {
 		}
 	}
 }
-/*
-Get the tiddler being edited and current value
-*/
-EditJsonWidget.prototype.getEditInfo = function() {
-	// Get the edit value
-	var self = this,
-		value,
-		update;
-	if(this.editIndex) {
-		value = this.wiki.extractTiddlerDataItem(this.editTitle,this.editIndex,this.editDefault);
-		update = function(value) {
-			var data = self.wiki.getTiddlerData(self.editTitle,{});
-			if(data[self.editIndex] !== value) {
-				data[self.editIndex] = value;
-				self.wiki.setTiddlerData(self.editTitle,data);
-			}
-		};
-	} else {
-		// Get the current tiddler and the field name
-		var tiddler = this.wiki.getTiddler(this.editTitle);
-		if(tiddler) {
-			// If we've got a tiddler, the value to display is the field string value
-			value = tiddler.getFieldString(this.editField);
-		} else {
-			// Otherwise, we need to construct a default value for the editor
-			value = '{}';
-		}
-
-		update = function(value) {
-			var tiddler = self.wiki.getTiddler(self.editTitle),
-				updateFields = {
-					title: self.editTitle
-				};
-			updateFields[self.editField] = value;
-			self.wiki.addTiddler(new $tw.Tiddler(self.wiki.getCreationFields(),tiddler,updateFields,self.wiki.getModificationFields()));
-		};
-	}
-	return {value: value, update: update};
-};
-
-/*
-Compute the internal state of the widget
-*/
-EditJsonWidget.prototype.execute = function() {
-	// Get our parameters
-	this.editTitle = this.getAttribute("tiddler",this.getVariable("currentTiddler"));
-	this.editField = this.getAttribute("field","text");
-	this.editIndex = this.getAttribute("index");
-	this.editClass = this.getAttribute("class");
-	this.editPlaceholder = this.getAttribute("placeholder");
-	this.editFocusPopup = this.getAttribute("focusPopup");
-	this.onkeyupdate = this.getAttribute("onkeyupdate","yes"); 
-	// Get the editor element tag and type
-	var tag,type;
-	if(this.editField === "text") {
-		tag = "textarea";
-	} else {
-		tag = "input";
-		var fieldModule = $tw.Tiddler.fieldModules[this.editField];
-		if(fieldModule && fieldModule.editTag) {
-			tag = fieldModule.editTag;
-		}
-		if(fieldModule && fieldModule.editType) {
-			type = fieldModule.editType;
-		}
-		type = type || "text";
-	}
-	// Get the rest of our parameters
-	this.editTag = this.getAttribute("tag",tag);
-	this.editType = this.getAttribute("type",type);
-	try {
-	 var data=$tw.wiki.getTiddlerData("$:/plugins/bj/jsoneditor/options.json");
-	 this.onkeyupdate=(data['onkeyupdate'])?data['onkeyupdate']:'yes';
-	 this.format=(data['format'])?data['format']:'';
-	} catch(e){ 
-		alert("invalid style format");
-		this.onkeyupdate="yes";
-		this.format='';
-	}
-};
-
-/*
-Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
-*/
-EditJsonWidget.prototype.refresh = function(changedTiddlers) {
-	var changedAttributes = this.computeAttributes();
-	// Completely rerender if any of our attributes have changed
-	if(changedAttributes.tiddler || changedAttributes.field || changedAttributes.index) {
-		this.refreshSelf();
-		return true;
-	} else if(changedTiddlers[this.editTitle]) {
-		//this.refreshSelf();//BJ FIXME saving cause the widget to get redrawn
-		return true;
-	}
-	return false;
-};
 
 
-/*
-Handle a dom "input" event
-*/
-EditJsonWidget.prototype.handleInputEvent = function(event) {
-	this.saveChanges(JSON.stringify(this.json));
-	return true;
-};
-
-EditJsonWidget.prototype.handleFocusEvent = function(event) {
-	if(this.editFocusPopup) {
-		$tw.popup.triggerPopup({
-			domNode: this.domNodes[0],
-			title: this.editFocusPopup,
-			wiki: this.wiki,
-			force: true
-		});
-	}
-	return true;
-};
-
-EditJsonWidget.prototype.saveChanges = function(text) {
-	var editInfo = this.getEditInfo();
-	if(text !== editInfo.value) {
-		editInfo.update(text);
-	}
-};
-
-exports["edit-json"] = EditJsonWidget;
 
 })();
